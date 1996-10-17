41430357721eaf537c1a3e9137db80e791c9709b
Christopher J. Madsen <perl@cjmweb.net>
1996-10-16 23:24:05 -0500

    Improve tests
    Test new '+-' interface to setAttribs
    Create and clean up test files
    Test both plain file and directory
    Test getting/setting attributes of nonexistent file

--- test.pl
+++ test.pl
@@ -1,19 +1,23 @@
-# $Id: test.pl,v 0.1 1996/10/17 01:55:12 Madsen Exp $
+# $Id: test.pl,v 0.2 1996/10/17 04:24:05 Madsen Exp $
 #
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
 
 BEGIN {
-    @getTest = (
-        'R____',
-        '_H___',
-        '__S__',
-        '___A_',
-        '____D',
-        'R__A_',
+    @tests = (
+        [ '',      '____' ],
+        [ '+r',    'R___' ],
+        [ '-r+h',  '_H__' ],
+        [ '+S-H',  '__S_' ],
+        [ '-s +A', '___A' ],    # Spaces are OK between groups
+        [ '+R',    'R__A' ],
+        [ 'H_S',   '_HS_' ],    # Underscores are OK,
+        [ 'h sar', '_H__' ],    # but don't use spaces in attribute list
+        [ 'ar',    'R__A' ],
+        [ '-ra',   '____' ]
     );
 
-    $last_test_to_print = 1 + scalar @getTest;
+    $last_test_to_print = 3 + 2 * scalar @tests;
 }
 
 ######################### We start with some black magic to print on failure.
@@ -33,24 +37,61 @@ print "ok 1\n";
 # (correspondingly "not ok 13") depending on the success of chunk 13
 # of the test code):
 
-system('cmd.exe', '/c', 'pretest.cmd');
-
+my $failed  = 0;
 my $testNum = 2;
 
-foreach (@getTest) { testGet($_) }
+my $testfile = 'DELETE.ME';
+unlink $testfile if -e $testfile;
 
-sub testGet
-{
-    my $attrib = shift;
+open(OUT,">$testfile") or die "Unable to create $testfile";
+close OUT;
+runTests($testfile, '_');       # Run tests on plain file
+unlink $testfile;
 
-    my $get = getAttribs("$attrib.TST");
-    if ($get eq $attrib) {
-        print "ok $testNum\n";
-    } else {
-        print "Expected '$attrib', got '$get'\nnot ok $testNum\n";
-    }
-    ++$testNum;
+mkdir($testfile, 0666) or die "Unable to create $testfile";
+runTests($testfile, 'D');       # Run tests on directory
+rmdir $testfile;
+
+# Make sure it fails for nonexistent path:
+if (getAttribs($testfile) || not $!) {
+    print "not ok $testNum\n";
+    ++$failed;
+} else {
+    $! = 0;
+    print "ok $testNum\n";
 }
+++$testNum;
+
+if (setAttribs('',$testfile) || not $!) {
+    print "not ok $testNum\n";
+    ++$failed;
+} else {
+    $! = 0;
+    print "ok $testNum\n";
+}
+
+if ($failed) { print "Failed $failed tests.\a\n" }
+else         { print "Passed.\n"                 }
+
+exit $failed;
+
+sub runTests
+{
+    my ($testfile, $type) = @_;
+
+    foreach (@tests) {
+        my $get;
+        $get = getAttribs($testfile) if setAttribs($$_[0], $testfile);
+
+        if ($get eq "$$_[1]$type") {
+            print "ok $testNum\n";
+        } else {
+            print "Expected '$$_[1]$type', got '$get'\nnot ok $testNum\n";
+            ++$failed;
+        }
+        ++$testNum;
+    }
+} # end runTests
 
 # Local Variables:
 # compile-command: "make test"
